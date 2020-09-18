cdc400f6b3184da8cbcf456ad7c6f838b4351e76
Lucas Cherobim <lucascherobim@hotmail.com>
2020-09-17 23:28:26 -0300

    ajuste navbar

--- partials/__navbar.php
+++ partials/__navbar.php
@@ -1,3 +1,6 @@
+<?php 
+
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="index.php">Portal ZOO</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
