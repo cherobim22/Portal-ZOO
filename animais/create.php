d6ed32ac5865b3d5160110ae8c112241ab007655
Lucas Cherobim <lucascherobim@hotmail.com>
2020-09-19 20:35:27 -0300

    update files

--- animais/create.php
+++ animais/create.php
@@ -1,17 +1,31 @@
 <?php
-
+require_once('../src/utils/ConnectionFactory.php');
 require_once('../src/dao/AnimaisDAO.php');
 
+// var_dump($_POST);
+// exit(0);
+
 $nome = $_POST['nome'];
+$imagem = $_POST['imagem'];
+$categoria = $_POST['categoria_id'];
 $alimentacao = $_POST['alimentacao'];
 $peso = $_POST['peso'];
-$categoria = $_POST['categoria_id'];
 $longevidade = $_POST['longevidade'];
 
 
-$stmt = AnimaisDAO::create($nome, $alimentacao, $categoria, $peso, $longevidade );
+$con = ConnectionFactory::getConnection();
+$stmt = $con->prepare("INSERT INTO animaisdd (img_animal, nome, categoria_id, peso, alimentacao, longevidade) 
+                                        VALUES ( :imagem, :nome, :categoria_id, :peso, :alimentacao, :longevidade)");
+$stmt->bindParam(":nome", $nome);
+$stmt->bindParam(":imagem", $imagem);
+$stmt->bindParam(":categoria_id", $categoria);
+$stmt->bindParam(":alimentacao", $alimentacao);
+$stmt->bindParam(":peso", $peso);
+$stmt->bindParam(":longevidade", $longevidade);
+$stmt->execute();
 
-header(("Location: animais"))
+
+header(("Location: animais"));
 
 
 ?>
