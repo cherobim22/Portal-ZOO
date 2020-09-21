be4721f80383a3bf542e90e8bbe25126cb0985ed
Lucas Cherobim <lucascherobim@hotmail.com>
2020-09-20 23:09:17 -0300

    update partials

--- partials/__navbar.php
+++ partials/__navbar.php
@@ -2,7 +2,7 @@
 
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="index.php">Portal ZOO</a>
+        <a class="navbar-brand" href="/app">Portal ZOO</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -18,21 +18,10 @@
                     <li class="nav-item">
                         <a class="nav-link" href="/categorias">Categorias</a>
                     </li>
-                    <!-- <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li> -->
                 </ul>
             </div>
 
             <span><?= $_SESSION['user'] ?></span><a class="nav-link" href="../login/sign_out.php">Sair</a>
-            
+           
         </div>           
     </nav>
--- partials/__sidebar.php
+++ partials/__sidebar.php
@@ -4,13 +4,30 @@ require_once('../src/dao/CategoriaDAO.php');
 
 $cat_stmt = CategoriaDAO::getAll();
 ?>
+
+<style>
+    .teste{
+        display: flex;
+        flex-direction: column;
+        padding: 5px;
+    }
+    a{
+        margin-top: 5px;
+    }
+    h2{
+        margin-left: 25%;
+    }
+</style>
 <aside class="col-md-3" >
                     <h2>Categorias</h2>
-                    <ul>
+                    <div class="teste">
                     <?php while($row = $cat_stmt->fetch(PDO::FETCH_OBJ)) : ?>
-                        <li><a href="/app/?categoria_id=<?= $row->id_categoria ?>"><?= $row->nome ?></a></li>
+                        <!-- <li><a href="/app/?categoria_id=<?= $row->id_categoria ?>"><?= $row->nome ?></a></li> -->
+                        <a type="button" href="../app/?categoria_id=<?= $row->id_categoria ?>" class="btn btn-outline-info"><?= $row->nome ?> </a>
                     <?php endwhile?>
-                    </ul>
+                    </div>
+               
+                   
 </aside>
 
 
