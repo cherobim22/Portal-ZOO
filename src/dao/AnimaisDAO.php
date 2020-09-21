43b45cffe0e8a8bc9b67f1edbc38911de2986080
Lucas Cherobim <lucascherobim@hotmail.com>
2020-09-20 23:10:27 -0300

    update DAO e styles

--- src/dao/AnimaisDAO.php
+++ src/dao/AnimaisDAO.php
@@ -6,19 +6,31 @@
        
         $con = ConnectionFactory::getConnection();
         
-        $stmt = $con->prepare("SELECT a.id as id, a.nome as nome, a.peso , a.alimentacao , a.longevidade, a.categoria_id, c.nome as categoria_nome FROM animais a JOIN portal_zoo.categorias c ON (a.categoria_id = c.id_categoria)");
+        $stmt = $con->prepare("SELECT a.id as id, a.nome as nome, a.img_animal, a.peso , a.alimentacao , a.longevidade, a.categoria_id, c.nome as categoria_nome FROM animaisdd a JOIN portal_zoo.categorias c ON (a.categoria_id = c.id_categoria)");
         $stmt->execute();
 
         return $stmt;
      }
+     public static function getById($id){
+       
+      $con = ConnectionFactory::getConnection();
+      
+      $stmt = $con->prepare("SELECT * FROM animaisdd WHERE id = :id");
+      $stmt->bindParam(":id", $id);
+      $stmt->execute();
+
+      return $stmt;
+   }
    
      
-     public static function create($nome, $alimentacao, $categoria, $peso, $longevidade){
+     public static function create($nome, $imagem, $alimentacao, $categoria, $peso, $longevidade){
         $con = ConnectionFactory::getConnection();
-        $stmt = $con->prepare("INSERT INTO animais (nome, peso, alimentacao, categoria_id, longevidade) VALUES (:nome, :peso, :alimentacao, :categoria_id, :longevidade)");
+        $stmt = $con->prepare("INSERT INTO animaisdd (img_animal, nome, categoria_id, peso, alimentacao, longevidade) 
+                                                VALUES ( :imagem, :nome, :categoria_id, :peso, :alimentacao, :longevidade)");
         $stmt->bindParam(":nome", $nome);
-        $stmt->bindParam(":alimentacao", $alimentacao);
+        $stmt->bindParam(":imagem", $imagem);
         $stmt->bindParam(":categoria_id", $categoria);
+        $stmt->bindParam(":alimentacao", $alimentacao);
         $stmt->bindParam(":peso", $peso);
         $stmt->bindParam(":longevidade", $longevidade);
         $stmt->execute();
@@ -26,13 +38,14 @@
         return $stmt;
      }
 
-     public static function update($id, $nome, $alimentacao, $categoria, $peso, $longevidade){
+     public static function update($id, $nome, $imagem, $alimentacao, $categoria, $peso, $longevidade){
 
         $con = ConnectionFactory::getConnection();
 
-        $stmt = $con->prepare("UPDATE animais SET nome=:nome, peso=:peso, alimentacao=:alimentacao, longevidade=:longevidade, categoria_id=:categoria_id WHERE id = :id");
+        $stmt = $con->prepare("UPDATE animaisdd SET nome=:nome, img_animal=:imagem, peso=:peso, alimentacao=:alimentacao, longevidade=:longevidade, categoria_id=:categoria_id WHERE id = :id");
         $stmt->bindParam(":id", $id);
         $stmt->bindParam(":nome", $nome);
+        $stmt->bindParam(":imagem", $imagem);
         $stmt->bindParam(":alimentacao", $alimentacao);
         $stmt->bindParam(":categoria_id", $categoria);
         $stmt->bindParam(":peso", $peso);
@@ -46,10 +59,18 @@
      public static function delete($id){
         $con = ConnectionFactory::getConnection();
 
-        $stmt = $con->prepare("DELETE FROM animais WHERE id = :id");
+        $stmt = $con->prepare("DELETE FROM animaisdd WHERE id = :id");
         $stmt->bindParam(":id", $id);
         $stmt->execute();
         return $stmt;
+     }
+
+     public static function getByCategory($categoria_id){
+         $con = ConnectionFactory::getConnection();
+         $stmt =  $con->prepare("SELECT * FROM animaisdd WHERE categoria_id=:id ");
+         $stmt->bindParam(":id", $categoria_id);
+         $stmt->execute();
+         return $stmt;
      }
 
  }
